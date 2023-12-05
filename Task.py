1532dcf813620e54302b46a400c201f0fa5aabca
QossayZeineddin <qossay.zeineddin@gmai.com>
2023-12-05 15:38:47 +0200

    Add partition

--- Task.py
+++ Task.py
@@ -1,7 +1,9 @@
+import os
+import requests
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, udf, lit, split
 from pyspark.sql.types import StringType
-import requests
 
 def start_spark_session():
     spark = SparkSession.builder.appName("CarOriginExtraction").getOrCreate()
@@ -11,69 +13,61 @@ def read_data_set_by_spark(spark, file_paths):
     dfs = [spark.read.option('header', 'true').csv(file_path) for file_path in file_paths]
     return tuple(dfs)
 
-
 def call_api_for_country(car_brand, api_url):
     try:
-        # Make a simple GET request to the API
         response = requests.get(api_url + f'/{car_brand}')
-
-        # Assuming the API returns a list of dictionaries
         json_response_list = response.json()
 
-        print(f'Response for {car_brand}: {json_response_list}')  # Add this line
-
-        # Check if the response is a non-empty list
         if json_response_list:
-            # Extract the first item from the list
             json_response = json_response_list[0]
-
-            # Extract the country of origin from the response
             country_of_origin = json_response.get('countryOfOrigin', 'Unknown')
         else:
-            # If the list is empty, set country_of_origin to 'Unknown'
             country_of_origin = 'Unknown'
 
         return country_of_origin
     except Exception as e:
-        # Handle exceptions gracefully
         return f'Error: {str(e)}'
 
-
-def extract_car_model_and_origin(spark, api_url, df_sheet, output_csv_path):
-    # Select only the relevant columns
+def extract_car_model_and_origin(spark, api_url, df_sheet, output_base_path):
     car_models = df_sheet.select(col('Make_Model')).distinct()
-    print(car_models.show())
 
-
-    # Add a new column 'Country_of_Origin' using the API
+    # Use withColumn and a lambda function to apply the UDF to each row
     car_models_with_origin = car_models.withColumn(
         'Country_of_Origin',
         call_api_for_country_udf(split(col('Make_Model'), ' ').getItem(0), lit(api_url))
     )
+    print(car_models_with_origin.show())
+    # Repartition by 'Country_of_Origin' for better parallelism
+    car_models_with_origin = car_models_with_origin.repartition('Country_of_Origin')
 
-    # Save the results to a new CSV file
-    car_models_with_origin.write.mode('overwrite').csv(output_csv_path)
+    # Collect distinct countries to process each partition separately
+    countries = [row.Country_of_Origin for row in car_models_with_origin.select('Country_of_Origin').distinct().collect()]
 
+    def write_partition_to_file(country):
+        country_df = car_models_with_origin.filter(col('Country_of_Origin') == country)
+        file_path = f'{output_base_path}/country_{country}.csv'
+        country_df.drop('Country_of_Origin').write.mode('overwrite').csv(file_path)
 
-
+    # Apply the function to each distinct country
+    for country in countries:
+        write_partition_to_file(country)
 
 # Convert the function to a UDF
 call_api_for_country_udf = udf(call_api_for_country, StringType())
 
 if __name__ == '__main__':
-    # Specify the file paths
     file_paths = ['taskfiles/cars.csv', 'taskfiles/Sheet1.csv', 'taskfiles/2015_State_Top10Report_wTotalThefts.csv']
+    api_url = 'http://127.0.0.1:8080/cars/getbyBrand'
+    output_base_path = 'taskfiles/output'
 
     spark_session = start_spark_session()
     dfs = read_data_set_by_spark(spark_session, file_paths)
     df_spark_car, df_spark_sheet, df_sparkTotalThefts = dfs
-    print(df_spark_car.printSchema())
+    print(df_sparkTotalThefts.printSchema())
 
-    # Replace this path with the actual path to your desired output location
-    output_csv_path = 'taskfiles/test'
+    if not os.path.exists(output_base_path):
+        os.makedirs(output_base_path)
 
-    # Replace 'http://localhost:8080/api/cars/search' with the actual URL of your API
-    api_url = 'http://127.0.0.1:8080/cars/getbyBrand'
 
-    extract_car_model_and_origin(spark_session, api_url, df_sparkTotalThefts, output_csv_path)
+    extract_car_model_and_origin(spark_session, api_url, df_sparkTotalThefts, output_base_path)
     print("Doneeee")
